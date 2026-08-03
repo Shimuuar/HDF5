d2a79c24f05298594b28e2d334c73e5b04141b84
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2026-08-03 18:26:41 +0300

    Configure TMP in nix shell

--- shell.nix
+++ shell.nix
@@ -6,6 +6,15 @@ let
     [ ipython
       h5py
     ]);
+  # Extra config
+  conf   = if builtins.pathExists ./conf.nix then import ./conf.nix else {};
+  tmpdir =
+    if builtins.hasAttr "tmpdir" conf
+    then ''
+      export TMPDIR=${conf.tmpdir}
+      export TMP=${conf.tmpdir}
+      ''
+    else "";
 in
 pkgs.mkShell {
   buildInputs = [
@@ -19,5 +28,6 @@ pkgs.mkShell {
   shellHook = ''
     export LD_LIBRARY_PATH=${pkgs.hdf5}/lib''${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH
     taskset -pc 0-1000 $$
+    ${tmpdir}
     '';
 }
