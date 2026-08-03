36368676c79a36aa8b7be09d473add580109aedd
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2026-08-03 21:59:29 +0300

    Oh Zeus!

--- HDF5-direct/cbits/hdf5.c
+++ HDF5-direct/cbits/hdf5.c
@@ -39,6 +39,7 @@ static int printing_disabled = 0;
 
 #define INI                                       \
     do {                                          \
+        printf("calling %s\n", __FUNCTION__);     \
         if( 0 == printing_disabled ) {            \
             H5Eset_auto(H5E_DEFAULT, NULL, NULL); \
             printing_disabled = 1;                \
